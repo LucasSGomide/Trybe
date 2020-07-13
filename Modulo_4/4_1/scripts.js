d226a22cd314df2cb11a4085f08166973fb98ffd
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-13 17:24:53 -0300

    Adicionando exercício 1 de JavaScript

--- Modulo_4/4_1/scripts.js
+++ Modulo_4/4_1/scripts.js
@@ -1,1 +1,30 @@
-let
+// Declaração de Variável
+
+// Forma de realizar comentários
+// Variáãveis são case sensitive
+// let é a forma atualizada de se declarar variavel
+// Sempre utilizar camelCase
+
+let idade = 5;
+console.log(idade)
+
+let nome = "Lucas";
+console.log(nome)
+
+let dia = 13, mes = "Julho", ano = 2020;
+console.log(dia, mes, ano)
+
+// Tipos Primitivos
+
+let string = "String"; // string literal
+let numero = 10; // number literal
+let estaAprovado = true; // Boolean 
+let sobrenome ; // Não declarada é undefined
+let corSelecionado = null; // nula, pode ser trocada
+
+// Comparações
+
+// == equal to
+// === equal value and type
+// != not equal
+// !== not equal value and type
