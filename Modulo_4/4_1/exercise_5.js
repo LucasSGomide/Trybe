8930c768de35af45c5a91bd48c1aa5a0c1ca94e9
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-13 17:52:51 -0300

    Adding exercise 5

---
+++ Modulo_4/4_1/exercise_5.js
@@ -0,0 +1,14 @@
+// Declarar variáveis
+
+let angA = 4;
+let angB = 7;
+let angC = 18;
+let angCheck = angA + angB +  angC
+
+// Programa que cheque se os angulos A B e C formam um triangulo:
+
+if (angCheck <= 180) {
+  console.log(angCheck <= 180)
+} else {
+  console.log("Error")
+}
