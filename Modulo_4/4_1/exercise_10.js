8dc2cb7fbcf56582b9e3ab90ad84a332aa2bb46f
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-13 20:31:39 -0300

    Adding exercise 10

---
+++ Modulo_4/4_1/exercise_10.js
@@ -0,0 +1,16 @@
+// Declaração de variáveis
+
+let productValue = 100;
+let sellingValue = 120;
+let profit = (sellingValue - productValue) * (0,8) * (1000)
+
+if (productValue > 0 && sellingValue > 0) {
+  console.log("Profit: R$ " + profit)
+} else {
+  console.log("Error")
+}
+
+
+
+
+
