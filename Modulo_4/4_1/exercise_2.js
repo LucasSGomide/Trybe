4da92ca2221066a86d77b208703db24c132d50bd
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-13 17:30:32 -0300

    Adding exercise 2

---
+++ Modulo_4/4_1/exercise_2.js
@@ -0,0 +1,15 @@
+// Declarar variáveis
+
+let numA = 4;
+let numB = 7;
+let maiorN = 0;
+
+// Programa que retorne o maior de dois numeros:
+
+if(numA > numB) {
+  maiorN = numA
+} else {
+  maiorN = numB
+}
+
+console.log(maiorN)
