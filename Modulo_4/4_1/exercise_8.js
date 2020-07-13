12861a1d0c67875dced65817a05e56b3ddb9a249
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-13 20:36:52 -0300

    Adding exercise 8

---
+++ Modulo_4/4_1/exercise_8.js
@@ -0,0 +1,17 @@
+// Declaração de variáveis
+
+let numA = 8;
+let numB = 10;
+let numC = 2;
+
+let isPair = false
+
+// Software that returns true if any of the variables is an even number.
+
+if ((numA % 2 === 0) || (numB % 2 === 0) || (numC % 2 === 0)) {
+  isPair = true
+} else {
+  isPair
+}
+
+console.log(isPair)
