a5446084b332e1e592435eef6940cd443dec94f9
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-16 18:00:15 -0300

    Adding exercise 1 parcially done

---
+++ Modulo_4/4_4/part2_ex1.js
@@ -0,0 +1,14 @@
+texto = "arara"
+
+function reverseString(texto) {
+  var reversedText = '';
+  for (let letra = texto.length - 1; letra >= 0; letra -= 1) {
+    reversedText += texto[letra];
+  }   
+  return reversedText;
+}
+
+
+function palindromoCheck(reversedText) {
+  if palavra === 
+}
