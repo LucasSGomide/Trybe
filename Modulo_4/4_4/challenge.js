9dab22aaa0f07ebdd642980ab7519d8f224a868f
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-16 23:34:34 -0300

    Adding roman algarisms challenge partially done

---
+++ Modulo_4/4_4/challenge.js
@@ -0,0 +1,30 @@
+let romanAlgarisms = {I : 1, IV : 4, V : 5, IX: 9, X : 10, XL : 40, L : 50, XC : 90, C : 100, CD : 400, D : 500, CM : 900, M : 1000,}
+
+// for (let index in romanAlgarisms) {
+//  console.log(index + " = " + romanAlgarisms[index])
+// }
+
+let wordTest = "IX"
+
+function romanAlgarismsConverter(wordTest) {
+   let firstAlgarism ;
+   let secondAlgarism;
+   let sum;
+
+  for (let index in romanAlgarisms) {
+    for (let cont = 0; cont < wordTest.length; cont += 1) {
+      if (wordTest[cont] == index && cont == 0) {
+        firstAlgarism = romanAlgarisms[index];
+      } else if (wordTest[cont] == index && cont == 1) {
+        secondAlgarism = romanAlgarisms[index]
+      }
+    }
+  }
+  if (firstAlgarism > secondAlgarism) {
+    sum = firstAlgarism + secondAlgarism
+  } else {
+    sum = secondAlgarism - firstAlgarism
+  }
+  return ("Primeiro algarismo: " + firstAlgarism + " segundo algarismo : " + secondAlgarism + " operação : " + sum)
+}
+console.log(romanAlgarismsConverter(wordTest))
