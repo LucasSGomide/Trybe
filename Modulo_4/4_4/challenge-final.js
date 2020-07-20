5bdb55fb5643c6fda24363dca91f067b8b3d2894
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-20 00:28:38 -0300

    Git Roman Algarisms Challenge (100%)

--- Modulo_4/4_4/challenge-final.js
+++ Modulo_4/4_4/challenge-final.js
@@ -6,6 +6,19 @@ let romanAlgarisms = {I : 1, V : 5, X : 10, L : 50, C : 100, D : 500, M : 1000,}
 
 let wordTest = "XCIV"
 
+let vetorRomano = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV',
+'XV','XVI','XVII','XVIII','XIX','XX','XXI','XXII','XXIII','XXIV','XXV','XXVI','XXVII','XXVIII',
+'XXIX','XXX','XXXI','XXXII','XXXIII','XXXIV','XXXV','XXXVI','XXXVII','XXXVIII','XXXIX','XL',
+'XLI','XLII','XLIII','XLIV','XLV','XLVI','XLVII','XLVIII','XLIX','L','LI','LII','LIII','LIV',
+'LV','LVI','LVII','LVIII','LIX','LX','LXI','LXII','LXIII','LXIV','LXV','LXVI','LXVII','LXVIII',
+'LXIX','LXX','LXXI','LXXII','LXXIII','LXXIV','LXXV','LXXVI','LXXVII','LXXVIII','LXXIX','LXXX',
+'LXXXI','LXXXII','LXXXIII','LXXXIV','LXXXV','LXXXVI','LXXXVII','LXXXVIII','LXXXIX','XC','XCI',
+'XCII','XCIII','XCIV','XCV','XCVI','XCVII','XCVIII','XCIX','C'];
+
+for (const key in vetorRomano) { 
+  console.log(romanAlgarismsConverter(vetorRomano[key]));
+}
+
 function romanAlgarismsConverter(wordTest) {
   let numberKeeper = [];
   let sum = 0;
@@ -25,7 +38,7 @@ function romanAlgarismsConverter(wordTest) {
       valueOne = (numberKeeper[cont] - numberKeeper[cont + 1])
       sum += valueOne
     } else if (numberKeeper[cont + 1] >= numberKeeper[cont]) {
-      console.log("Esquerda : " + numberKeeper[cont + 1] + " Direita : " + numberKeeper[cont])
+      // console.log("Esquerda : " + numberKeeper[cont + 1] + " Direita : " + numberKeeper[cont])
       valueTwo = (numberKeeper[cont + 1] + numberKeeper[cont])
       sum += valueTwo
     }
@@ -34,7 +47,10 @@ function romanAlgarismsConverter(wordTest) {
     sum -= numberKeeper[lastNumber]
   } else if (numberKeeper.length % 2 != 0 && numberKeeper[lastNumber -1] <= numberKeeper[lastNumber]) {
     sum += numberKeeper[lastNumber]
+  } else if (numberKeeper.length == 1) {
+    sum += numberKeeper[0]
   }
-  return ("lista : " + numberKeeper + " soma : " + sum)
+
+  return ("Algarismos : " + numberKeeper + " | Soma : " + sum)
 }
-console.log(romanAlgarismsConverter(wordTest))
+// console.log(romanAlgarismsConverter(wordTest))
