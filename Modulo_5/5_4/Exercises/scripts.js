0598ed3c66222e5f86d75fe09e5a96c1dfa4434a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-25 13:58:30 -0300

    Adding article blog exercise with color-selector, font-size, font-selector and loading user settings automatically

--- Modulo_5/5_4/Exercises/scripts.js
+++ Modulo_5/5_4/Exercises/scripts.js
@@ -36,4 +36,34 @@ window.onload = function() {
     document.body.style.color = textColor.value 
   })
 
+  fontSelector = document.querySelector("#font-selector")
+
+  fontSelector.addEventListener("change", function(event) {
+    document.body.style.fontFamily = fontSelector.value
+  })
+
+  let saveSettings = document.querySelector("#save-settings")
+
+  saveSettings.addEventListener("click", function(event) {
+    localStorage.setItem("font-size",article.style.fontSize)
+    localStorage.setItem("line-height",article.style.lineHeight)
+    localStorage.setItem("background-color",document.body.style.backgroundColor )
+    localStorage.setItem("text-color", document.body.style.color)
+    localStorage.setItem("font-family",document.body.style.fontFamily)
+  })
+
+  function loadSettings() {
+    if (localStorage.length > 0) {
+      article.style.fontSize = localStorage.getItem("font-size")
+      
+      article.style.lineHeight = localStorage.getItem("line-height")
+
+      document.body.style.backgroundColor = localStorage.getItem("background-color")
+
+      document.body.style.color = localStorage.getItem("text-color")
+
+      document.body.style.fontFamily = localStorage.getItem("font-family")
+    }
+  }
+  loadSettings()  
 }
