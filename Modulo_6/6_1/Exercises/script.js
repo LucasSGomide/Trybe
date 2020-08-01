410a00ae851555e6333b7334fa0ffbd84bf99c52
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-08-01 19:21:15 -0300

    Adding updates on exercises

--- Modulo_6/6_1/Exercises/script.js
+++ Modulo_6/6_1/Exercises/script.js
@@ -21,4 +21,36 @@ window.onload = function () {
 
   generateOptionStates (brazilianStates)
 
+  let submitForm = document.querySelector('#submit-form-button');
+  let dateForm = document.querySelector('#starting-date-form');
+  let applicationForm = document.querySelector('.application-form');
+  let formConfirmation = document.querySelector('.form-confirmation');
+
+  submitForm.addEventListener('click', function () {
+    let formSaver = applicationForm.innerHTML
+    let createDiv = document.createElement('div');
+    createDiv.className = 'confirm-form';
+    formConfirmation.appendChild(createDiv);
+    createDiv.appendChild(formSaver);
+  });
+
+  dateForm.addEventListener('input' , function () {
+    let dateSaver = '';
+    dateSaver = (dateForm.value);
+    let dateDay = parseInt(dateSaver.slice(0, 2));
+    let dateMonth = parseInt(dateSaver.slice(3, 5));
+    let dateYear = parseInt(dateSaver.slice(6, 10));
+    if (dateDay < 0 || dateDay > 31) {
+      console.log(dateDay) 
+      dateForm.value = '';             
+      alert('Invalid Sintax');
+    } else if (dateMonth < 0 || dateMonth > 12) {
+      alert('Invalid Sintax');
+      dateForm.value = '';
+    } else if (dateYear < 0) {
+      dateForm.value = '';
+      alert('Invalid Sintax');
+    }
+  });
+
 };
