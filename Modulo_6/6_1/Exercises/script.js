2eec5c932cc061b531caf5c2f614405d34f620f6
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-08-01 00:48:33 -0300

    Adding new info

--- Modulo_6/6_1/Exercises/script.js
+++ Modulo_6/6_1/Exercises/script.js
@@ -1,1 +1,24 @@
 // Form scripts
+window.onload = function () {
+
+  let stateForm = document.querySelector('#state-form');
+
+  let brazilianStates = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  function generateOptionTag () {
+    let optionTag = document.createElement('option');
+    return optionTag;
+  };
+  
+  function generateOptionStates (states) {
+    let newState = '';
+    for (index in states) {
+      newState = generateOptionTag();
+      newState.innerText = states[index];
+      stateForm.appendChild(newState);
+    }
+  };
+
+  generateOptionStates (brazilianStates)
+
+};
