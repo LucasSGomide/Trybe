75cc409c1e8c5b4b9e4470bbff87777aae877569
Lucas Santiago <lucas.sgomide92@gmail.com>
2020-08-20 18:19:17 -0300

    Adding live lecture files about HOFs

---
+++ Modulo_8/8_4/lesson/testing_rest.js
@@ -0,0 +1,25 @@
+const personObj = {
+  pessoa1: {nome: 'lucas', idade: 28},
+  pessoa2: {nome: 'andré', idade: 22},
+}
+
+
+const {nome, idade} = personObj
+
+console.log(personObj.pessoa2)
+
+
+const test = {
+  jobs: ['engraxate', 'marinheiro', 'x', 'y', 'z']
+};
+
+const {jobs} = test
+const newArr = Object.values(jobs)
+
+
+//console.log(Object.values(jobs))
+function organizeJob(x, y, z) {
+  console.log(`Ele foi ${x} também ${y} e ${z}`) 
+}
+
+organizeJob(...newArr)
