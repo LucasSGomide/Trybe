e9020357c59541bd5453fc9b59f602d4868345f6
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-10-23 15:29:20 -0300

    Adding module 16 - React-Redux structure

---
+++ Modulo_16/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// o provider é o meio pelo qual disponibilizamos o Store
+import { Provider } from 'react-redux';
+import store from './store';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          // componentes aqui
+        </Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
