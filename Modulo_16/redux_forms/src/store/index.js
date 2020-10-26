d83500d7a6d5ef8fd70f01ef92d64261eb5b65da
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-10-26 19:05:31 -0300

    Adding redux-dev-tools on store

--- Modulo_16/redux_forms/src/store/index.js
+++ Modulo_16/redux_forms/src/store/index.js
@@ -19,6 +19,6 @@ function reducer(state = intialState, action) {
 
 const rootReducer = combineReducers({ reducer });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
